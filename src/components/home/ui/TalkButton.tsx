7d16f65adf1081c950a30977873ae9dc6efbaeb2
barakatimothy <timothybaraka39@gmail.com>
2025-05-19 13:31:43 +0300

    nude brown theme and pic

--- src/components/home/ui/TalkButton.tsx
+++ src/components/home/ui/TalkButton.tsx
@@ -4,7 +4,7 @@ import Strings from "@/constants/strings";
 const TalkButton = () => {
   return (
     <Link
-      className="app__filled_btn min-w-[10rem]"
+      className="min-w-[10rem] px-6 py-3 rounded-md font-semibold text-white bg-[#8b38d7] hover:bg-[#762bc4] transition-colors duration-300"
       href={Strings.linkedInLink}
       target="_blank"
     >
